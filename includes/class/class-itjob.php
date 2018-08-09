da376f101f2b7bb5c8d699116a6a21230474b878
Tiafeno <tiafenofnel@gmail.com>
2018-08-09 23:41:44 +0300

    init itjob class

--- includes/class/class-itjob.php
+++ includes/class/class-itjob.php
@@ -1,6 +1,12 @@
 <?php
-class itJob {
-  public function __construct(){}
+if (!defined('ABSPATH')) exit;
+if (!class_exists('itJob')) {
+  class itJob {
+    public function __construct(){
+      add_action('wp_loaded', function() {}, 20);
+    }
+  }
 }
+
 return new itJob();
 ?>
