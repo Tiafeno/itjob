1a2d4651facdb6a497ed71d30d0dcb97dd1bc0e9
Tiafeno <tiafenofnel@gmail.com>
2018-08-10 19:24:24 +0300

    added enqueue

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *   Copyright (c) 2018, Falicrea
  *
@@ -13,14 +14,14 @@
  *   copies or substantial portions of the Software.
  */
 
-define( '__SITENAME__', 'itJob' );
-define( '__google_api__', base64_decode( 'QUl6YVN5Qng3LVJKbGlwbWU0YzMtTGFWUk5oRnhiV19xWG5DUXhj' ) );
-define( 'TWIG_TEMPLATE_PATH', get_template_directory() . '/templates' );
-$theme = wp_get_theme( 'itjob' );
+define('__SITENAME__', 'itJob');
+define('__google_api__', base64_decode('QUl6YVN5Qng3LVJKbGlwbWU0YzMtTGFWUk5oRnhiV19xWG5DUXhj'));
+define('TWIG_TEMPLATE_PATH', get_template_directory() . '/templates');
+$theme = wp_get_theme('itjob');
 
-$itJob = (object) [
-	'version'  => $theme->get( 'Version' ),
-	'root'     => require 'includes/class/class-itjob.php'
+$itJob = (object)[
+  'version' => $theme->get('Version'),
+  'root' => require 'includes/class/class-itjob.php'
 ];
 
 require 'includes/class/class-menu-walker.php';
@@ -32,49 +33,63 @@ require 'jobs/itjob-cron.php';
 require 'composer/vendor/autoload.php';
 
 try {
-	$loader = new Twig_Loader_Filesystem();
-	$loader->addPath( TWIG_TEMPLATE_PATH . '/vc', 'VC' );
+  $loader = new Twig_Loader_Filesystem();
+  $loader->addPath(TWIG_TEMPLATE_PATH . '/vc', 'VC');
 
-	/** @var Object $Engine */
-	$Engine = new Twig_Environment( $loader, array(
-		'debug'       => WP_DEBUG,
-		'cache'       => TWIG_TEMPLATE_PATH . '/cache',
-		'auto_reload' => WP_DEBUG
-	) );
+  /** @var Object $Engine */
+  $Engine = new Twig_Environment($loader, array(
+    'debug' => WP_DEBUG,
+    'cache' => TWIG_TEMPLATE_PATH . '/cache',
+    'auto_reload' => WP_DEBUG
+  ));
 
 	// Ajouter des filtres
-	itjob_filter_engine( $Engine );
+  itjob_filter_engine($Engine);
 
-} catch ( Twig_Error_Loader $e ) {
-	die( $e->getRawMessage() );
+} catch (Twig_Error_Loader $e) {
+  die($e->getRawMessage());
 }
 
-add_action( 'after_setup_theme', function () {
-	load_theme_textdomain( 'twentyfifteen' );
-	load_theme_textdomain( __SITENAME__, get_template_directory() . '/languages' );
+add_action('after_setup_theme', function () {
+  load_theme_textdomain('twentyfifteen');
+  load_theme_textdomain(__SITENAME__, get_template_directory() . '/languages');
 
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'category-thumbnails' );
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'title-tag' );
-	add_theme_support( 'custom-logo', array(
-		'height'     => 100,
-		'width'      => 250,
-		'flex-width' => true,
-	) );
+  /** @link https://codex.wordpress.org/Post_Thumbnails */
+  add_theme_support('post-thumbnails');
+  add_theme_support('category-thumbnails');
+  add_theme_support('automatic-feed-links');
+  add_theme_support('title-tag');
+  add_theme_support('custom-logo', array(
+    'height' => 100,
+    'width' => 250,
+    'flex-width' => true,
+  ));
+
+ /*  
+  add_image_size('sidebar-thumb', 120, 120, true);
+  add_image_size('homepage-thumb', 220, 180);
+  add_image_size('singlepost-thumb', 590, 9999);
+  */
+
+  /**
+   * This function will not resize your existing featured images. 
+   * To regenerate existing images in the new size, 
+   * use the Regenerate Thumbnails plugin.
+   */
+  set_post_thumbnail_size( 50, 50, array( 'center', 'center')  ); // 50 pixels wide by 50 pixels tall, crop from the center
 
 	// Register menu location
-	register_nav_menus( array(
-		'primary'  => __( 'Primary Menu', 'twentyfifteen' ),
-		'menu-top' => __( 'Top Menu', __SITENAME__ )
-	) );
-} );
+  register_nav_menus(array(
+    'primary' => __('Primary Menu', 'twentyfifteen'),
+    'menu-top' => __('Top Menu', __SITENAME__)
+  ));
+});
 
-if( function_exists('acf_add_options_page') ) {
-	$parent = acf_add_options_page(array(
-		'page_title' 	=> 'General Settings',
-		'menu_title' 	=> 'itJob Settings',
-		'capability' => 'edit_posts',
-		'redirect' 		=> false
-	));
+if (function_exists('acf_add_options_page')) {
+  $parent = acf_add_options_page(array(
+    'page_title' => 'General Settings',
+    'menu_title' => 'itJob Settings',
+    'capability' => 'edit_posts',
+    'redirect' => false
+  ));
 }
--- includes/class/class-itjob.php
+++ includes/class/class-itjob.php
@@ -1,9 +1,37 @@
 <?php
 if (!defined('ABSPATH')) exit;
 if (!class_exists('itJob')) {
-  class itJob {
-    public function __construct(){
-      add_action('wp_loaded', function() {}, 20);
+  class itJob
+  {
+    public function __construct()
+    {
+      add_action('wp_loaded', function () {
+      }, 20);
+
+      add_action('init', function () {
+      });
+
+      add_action('widgets_init', function () {});
+
+      add_action('wp_enqueue_scripts', function () {
+        global $itJob;
+        /** Styles */
+        // Load components semantic ui stylesheet
+
+        // Load uikit stylesheet
+        wp_enqueue_style('uikit', get_template_directory_uri() . '/assets/css/uikit.min.css', '', '3.0.0rc10');
+        // Load the main stylesheet
+        wp_enqueue_style('itjob', get_stylesheet_uri(), '', $itJob->version);
+
+        /** Scripts */
+        wp_enqueue_script('underscore');
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('numeral', get_template_directory_uri() . '/assets/js/numeral.min.js', array(), $itJob->version, true);
+        wp_enqueue_script('bluebird', get_template_directory_uri() . '/assets/js/bluebird.min.js', array(), $itJob->version, true);
+        wp_enqueue_script('uikit', get_template_directory_uri() . '/assets/js/uikit.min.js', array('jquery'), $itJob->version, true);
+        wp_enqueue_script('uikit-icon', get_template_directory_uri() . '/assets/js/uikit-icons.min.js', array('jquery'), $itJob->version, true);
+      });
+
     }
   }
 }
