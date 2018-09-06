71479ddcab22a43ab72cb4dd1195ffd95a03cec8
Tiafeno <tiafenofnel@gmail.com>
2018-09-06 23:46:11 +0300

    Added footer (ttile, list group)

--- footer.php
+++ footer.php
@@ -1,3 +1,48 @@
-  <?php wp_footer(); ?>
+<footer class="uk-section uk-section-secondary">
+  <div class="uk-container uk-container-medium">
+    <div class="row mt-4 mb-5">
+      <div class="col-md-4 col-sm-6">
+        <div class="footer-body">
+          <h4 class="footer-title d-inline-block">MENU</h4>
+          <ul class="list-group footer-menu">
+            <li><a href="#">Les offres d'emploi</a></li>
+            <li><a href="#">Déposer votre CV</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6"></div>
+      <div class="col-md-4 col-sm-6"></div>
+    </div>
+  </div>
+</footer>
+<style type="text/css">
+  .footer-title {
+    margin-bottom: 15px;
+  }
+
+  ul.footer-menu li a {
+    padding-top: 3px;
+    padding-bottom: 3px;
+    display: block;
+  }
+
+  ul.footer-menu li a:hover {
+    color: #7ac943 !important;
+  }
+
+  ul.footer-menu li {
+    list-style: none;
+  }
+
+  h4.footer-title::after {
+    margin-top: 2px;
+    width: 50%;
+    content: " ";
+    display: block;
+    height: 1px;
+    background-color: #7AC943;
+  }
+</style>
+<?php wp_footer(); ?>
   </body>
 </html>
