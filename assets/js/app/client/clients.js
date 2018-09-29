06bf60bc839a473ebbfe9c90eb1cb1ff41a93cf9
Tiafeno <tiafenofnel@gmail.com>
2018-09-29 13:59:23 +0300

    Minor Fix

--- assets/js/app/client/clients.js
+++ assets/js/app/client/clients.js
@@ -129,7 +129,7 @@ angular.module('clientApp', ['ngMessages', 'froala', 'ngTagsInput', 'ngSanitize'
               }
             });
         };
-        UIkit.util.on('#modal-edit-user-overflow', 'hide', function (e) {
+        UIkit.util.on('#modal-edit-candidate-overflow', 'hide', function (e) {
           e.preventDefault();
           e.target.blur();
           $scope.status = false;
