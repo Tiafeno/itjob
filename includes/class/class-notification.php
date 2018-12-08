a62a5a381b5c4293d01d590daebe08ac8646baa8
Tiafeno <tiafenofnel@gmail.com>
2018-12-08 19:55:55 +0300

    Added class notification

--- includes/class/class-notification.php
+++ includes/class/class-notification.php
@@ -4,23 +4,57 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
-trait Types {
-  public $lists = [
-    "confirmation_publish_offer", // Confirmation de publication d'offre
-    "upgrade_account", // Notification pour la nature du compte
-    "alert_new_user", // Alert si un utlisateur vient d'etre publier sur la même secteur d'activité que cette entreprise
-    "alert_publication", // Alerte en générale (offre, candidat, etc..)
-    "candidate_postuled" // Si un candiat à postuler
-  ];
-}
 
 final class Notification {
-  use Types;
-  /** @var int $code - Contient le type du notification */
-  public $code;
-  public $title;
+  private $title;
+  private $message;
+  private $type;
+  private $status; // lu ou non lu
+  public $url;
 
-  public function __construct() {
+  public function __construct( $type = null) {
+    if (is_null($type) || empty($type)) return false;
   }
 
+  public function get_message() {
+    return $this->message;
+  }
+
+  public function get_title() {
+    return $this->title;
+  }
+
+  public static function getInstance($type = null) {
+    if (is_null($type) || empty($type) ) return false;
+    return new Notification($type);
+  }
+
+
 }
+
+final class NotificationHelper {
+  public function __construct() {
+    add_action('init', function () {
+
+    });
+  }
+
+  
+  public function notification_publish_cv( $id_cv ) {
+
+  }
+
+  public function notification_candidate_postuled( $id_cv, $id_offer ) {
+
+  }
+
+  public function notification_company_interest( $id_cv, $id_company ) {
+
+  }
+
+  public function notification_change_interet_status( $id_interest, $status ) {
+
+  }
+}
+
+new NotificationHelper();
