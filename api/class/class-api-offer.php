f22b57bfdd5d708d79da9a92fc9cae79e9af309f
Tiafeno <tiafenofnel@gmail.com>
2018-12-16 19:02:59 +0100

    Added filter post to find offer

--- api/class/class-api-offer.php
+++ api/class/class-api-offer.php
@@ -3,6 +3,50 @@
 final class apiOffer {
   public function __construct() {}
 
+
+    private function add_filter_search($search, $meta_query = [])
+    {
+      add_filter('posts_where', function ($where) use ($search, $meta_query) {
+        global $wpdb;
+        //global $wp_query;
+        $s = $search;
+        $where .= " AND {$wpdb->posts}.ID IN (
+                        SELECT
+                          pt.ID
+                        FROM {$wpdb->posts} as pt
+                        INNER JOIN {$wpdb->postmeta} as pm1 ON (pt.ID = pm1.post_id)
+                        WHERE pt.post_type = 'offers' ";
+
+        foreach ($meta_query as $meta) {
+          $meta = (object) $meta;
+          $where .=   "  AND (pt.ID IN (
+                            SELECT {$wpdb->postmeta}.post_id as post_id
+                            FROM {$wpdb->postmeta}
+                            WHERE {$wpdb->postmeta}.meta_key = '{$meta->meta_key}' AND {$wpdb->postmeta}.meta_value {$meta->meta_compare} {$meta->meta_value}
+                          ))";
+        }
+        $where .= ")"; //  .end AND
+          // Si une taxonomie n'est pas definie on ajoute cette condition dans la recherche
+        $where .= "  OR (
+                          {$wpdb->posts}.post_title LIKE  '%{$s}%'
+                          AND {$wpdb->posts}.post_type = 'offers'";
+
+        foreach ($meta_query as $meta) {
+          $meta = (object) $meta;
+          if (strpos($meta->meta_key, '%')) continue;
+          $where .=     " AND ({$wpdb->posts}.ID IN (
+                            SELECT {$wpdb->postmeta}.post_id as post_id
+                            FROM {$wpdb->postmeta}
+                            WHERE {$wpdb->postmeta}.meta_key = '{$meta->meta_key}' AND {$wpdb->postmeta}.meta_value {$meta->meta_compare} {$meta->meta_value}
+                          ))";
+        }
+
+        $where .=      ")";
+        return $where;
+      });
+    }
+  
+
   public function get_offers() {
     $length = (int)$_POST['length'];
     $start = (int)$_POST['start'];
@@ -19,13 +63,28 @@ final class apiOffer {
     if (isset($_POST['search']) && !empty($_POST['search']['value'])) {
       $search = stripslashes($_POST['search']['value']);
       $searchs = explode('|', $search);
-      if (!empty($searchs[0]) && $searchs[0] !== ' ')
-        $args = array_merge($args, ['s' => $searchs[0]]);
-      // activation
+      $s = '';
+      $meta_query = [];
       $activated = trim($searchs[1]) !== '' &&  trim($searchs[1]) !== ' ' ? (int)$searchs[1] : '';
+
       if ($activated === 1 || $activated === 0) {
-        $meta_query = ['meta_key' => 'activated', 'meta_value' => (int)$activated, 'meta_compare' => '='];
-        $args = array_merge($args, $meta_query);
+        $meta_query[] = [
+          'meta_key' => 'activated', 
+          'meta_value' => (int)$activated, 
+          'meta_compare' => '='
+        ];
+      }
+      if (!empty($s)) {
+        $meta_query[] = [
+          'meta_key' => 'itjob_offer_reference',
+          'meta_value' =>  "'%$s%'",
+          'meta_compare' => "LIKE"
+        ];
+      }
+      
+      if (!empty($searchs[0]) && $searchs[0] !== ' ') {
+        $s = $searchs[0];
+        $this->add_filter_search($s, $meta_query);
       }
 
       if (!empty($searchs[2]) && $searchs[2] !== ' ') {
