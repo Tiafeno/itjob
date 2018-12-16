7fb455ab1152f758c4e31e333726d0b7cd7a7ea8
Tiafeno <tiafenofnel@gmail.com>
2018-12-16 20:38:15 +0100

    test

--- api/class/class-api-offer.php
+++ api/class/class-api-offer.php
@@ -11,20 +11,20 @@ final class apiOffer {
         //global $wp_query;
         $s = $search;
         $where .= " AND {$wpdb->posts}.ID IN (
-                        SELECT
-                          pt.ID
-                        FROM {$wpdb->posts} as pt
-                        WHERE  
-                         pt.ID IN (
-                            SELECT {$wpdb->postmeta}.post_id as post_id
-                            FROM {$wpdb->postmeta}
-                            WHERE {$wpdb->postmeta}.meta_key = 'itjob_offer_reference' AND {$wpdb->postmeta}.meta_value LIKE '%{$s}%'
-                         )
-                         OR (pt.ID IN (
-                            SELECT {$wpdb->postmeta}.post_id as post_id
-                            FROM {$wpdb->postmeta}
-                            WHERE {$wpdb->postmeta}.meta_key = 'itjob_offer_post' AND {$wpdb->postmeta}.meta_value LIKE '%{$s}%'
-                          ))";
+                      SELECT
+                        pt.ID
+                      FROM {$wpdb->posts} as pt
+                      WHERE  
+                        pt.ID IN (
+                          SELECT {$wpdb->postmeta}.post_id as post_id
+                          FROM {$wpdb->postmeta}
+                          WHERE {$wpdb->postmeta}.meta_key = 'itjob_offer_reference' AND {$wpdb->postmeta}.meta_value LIKE '%{$s}%'
+                        )
+                        OR (pt.ID IN (
+                          SELECT {$wpdb->postmeta}.post_id as post_id
+                          FROM {$wpdb->postmeta}
+                          WHERE {$wpdb->postmeta}.meta_key = 'itjob_offer_post' AND {$wpdb->postmeta}.meta_value LIKE '%{$s}%'
+                        ))";
         $where .= ")"; //  .end AND
 
         return $where;
@@ -36,7 +36,7 @@ final class apiOffer {
     $length = (int)$_POST['length'];
     $start = (int)$_POST['start'];
     $paged = isset($_POST['start']) ? ($start === 0) ? 0 : $start / $length : 1;
-    $posts_per_page = isset($_POST['length']) ? (int)$_POST['length'] : 10;
+    $posts_per_page = isset($_POST['length']) ? (int)$_POST['length'] : 20;
     $args = [
       'post_type' => 'offers',
       'post_status' => ['publish', 'pending'],
@@ -48,8 +48,8 @@ final class apiOffer {
       $searchs = explode('|', $search);
       $s = '';
       $meta_query = [];
-      $activated = trim($searchs[1]) !== '' &&  trim($searchs[1]) !== ' ' ? (int)$searchs[1] : '';
 
+      $activated = trim($searchs[1]) !== '' &&  trim($searchs[1]) !== ' ' ? (int)$searchs[1] : '';
       if ($activated === 1 || $activated === 0) {
         $activatedArg = [
           'meta_key' => 'activated', 
@@ -75,7 +75,7 @@ final class apiOffer {
         $the_query->the_post();
         if (!is_array($the_query->posts)) return false;
         $offers = array_map(function ($offer) {
-          if (!isset($offer->ID)) return $offer;
+          if (!isset($offer->ID)) return false;
           $objOffer = new \includes\post\Offers($offer->ID, true);
 
           return $objOffer;
