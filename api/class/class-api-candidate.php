b5ff55c489967a390c760f6dbb21e746e8deddda
Tiafeno <tiafenofnel@gmail.com>
2018-12-17 19:09:17 +0100

    Test 3

--- api/class/class-api-candidate.php
+++ api/class/class-api-candidate.php
@@ -115,8 +115,7 @@ final class apiCandidate
         $tax_query[] = [
           'taxonomy' => 'branch_activity',
           'field' => 'term_id',
-          'terms' => [$activityArea],
-          'operator' => 'IN'
+          'terms' => [$activityArea]
         ];
       }
       
@@ -130,7 +129,6 @@ final class apiCandidate
     $args = array_merge($args, ['meta_query' => $meta_query]);
     $args = array_merge($args, ['tax_query' => $tax_query]);
 
-    print_r($args);
     $the_query = new WP_Query($args);
     $candidates = [];
     if ($the_query->have_posts()) {
