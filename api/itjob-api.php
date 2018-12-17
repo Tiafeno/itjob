b421578bef207b9d744371f9b18cd2a17435a7fa
Tiafeno <tiafenofnel@gmail.com>
2018-12-17 08:59:52 +0100

    Fix route

--- api/itjob-api.php
+++ api/itjob-api.php
@@ -272,7 +272,7 @@ add_action('rest_api_init', function () {
     )
   ]);
 
-  register_rest_route('ati-api', '/dashboard/', [
+  register_rest_route('it-api', '/dashboard/', [
     array(
       'methods' => WP_REST_Server::READABLE,
       'callback' => function (WP_REST_Request $request) {
