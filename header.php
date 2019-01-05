1e67185efbd5b5ef89a51316441fee4c132de182
Tiafeno <tiafenofnel@gmail.com>
2019-01-05 19:55:45 +0300

    Remove cookie message

--- header.php
+++ header.php
@@ -188,16 +188,6 @@
   your browser</a> to improve your experience.</p>
 <![endif]-->
 <div class="uk-offcanvas-content">
-
-  <div class="alert alert-info alert-dismissable fade show has-icon mb-0"><i class="la la-info alert-icon"></i>
-    <div class="d-flex align-items-center justify-content-between">
-      <div>Ceci est une version bêta du site ITJobMada. En utilisant ce site, vous acceptez que les cookies soient utilisés à des fins d'analyse, de pertinence et de publicité.</div>
-      <div>
-        <button class="btn btn-sm btn-info btn-rounded" data-dismiss="alert">Fermer</button>
-      </div>
-    </div>
-  </div>
-
 <div class="uk-section uk-section-small uk-padding-remove">
 
   <div class="uk-section uk-section-small uk-padding-remove header-top">
