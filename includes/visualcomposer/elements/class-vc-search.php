25cbfe2777688eadab462dc535563aa6798341fd
Tiafeno <tiafenofnel@gmail.com>
2019-02-28 02:41:32 +0300

    Minor fix (url)

--- includes/visualcomposer/elements/class-vc-search.php
+++ includes/visualcomposer/elements/class-vc-search.php
@@ -280,12 +280,13 @@ if ( ! class_exists( 'vcSearch' ) ):
         global $posts;
         $search_query   = Http\Request::getValue( 's' );
         $publish_ad_link = get_the_permalink( ADD_ANNONCE_PAGE );
+        $type = $args['post_type'] === 'annonce' ? 2 : 1;
         $args           = array_merge( $args, [
           's'              => $search_query,
           'ab'             => Http\Request::getValue('ab', ''),
           'rg'             => Http\Request::getValue('rg', ''),
           'search_count'   => count( $posts ),
-          'publish_ad_link' => $publish_ad_link
+          'publish_ad_link' => $publish_ad_link . '?type=' . $type
         ] );
         return $Engine->render( '@VC/search/search-annonce.html.twig', $args );
       } catch ( Twig_Error_Loader $e ) {
