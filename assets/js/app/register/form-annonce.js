243180cbc277b0aa7b9069cf01e7dd1ca7804eaf
Tiafeno <tiafenofnel@gmail.com>
2019-02-28 13:58:58 +0300

    Apply filter term name

--- assets/js/app/register/form-annonce.js
+++ assets/js/app/register/form-annonce.js
@@ -167,7 +167,6 @@ var companyApp = angular.module('AnnonceApp', ['ui.router', 'ngMessages', 'ui.ti
               return false;
             };
 
-
             const fileFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
             /**
              * Cette fonction permet de redimensionner une image
