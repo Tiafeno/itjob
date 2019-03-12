34c091af5399e9ff8d5a9b6cf0f4d6f6c5b754f2
Tiafeno <tiafenofnel@gmail.com>
2019-03-12 11:49:44 +0300

    Add & Fix custom column in WP admin

--- functions.php
+++ functions.php
@@ -270,18 +270,37 @@ add_action('init', function () {
     return $title;
   }, PHP_INT_MAX);
 
-//  $Model = new includes\model\itModel();
-//  add_action('repair_table', [$Model, 'repair_table'], 10);
 
-  add_filter('manage_formation_posts_columns', function ($columns) {
+  /**
+   *
+   * Combined with the manage_{$post_type}_posts_columns filter,
+   * this allows you to add or remove (unset) custom columns to the list
+   * post/page/custom post type pages (which automatically appear in Screen Options).
+   *
+   */
+  add_filter('manage_posts_columns', function ($columns) {
     return array_merge($columns,
-      array('activated' => __('Activation')));
+      array('activated' => __('Activé'))
+    );
+  });
+
+  add_filter('manage_wallets_posts_columns', function ($columns) {
+    return array_merge($columns,
+      array('wallet' => __('Credit'))
+    );
   });
 
   add_action('manage_posts_custom_column', function ($column, $post_id) {
     $activate = get_field('activated', $post_id);
-    if ($column == 'activated') {
-      echo '<input type="checkbox" disabled', $activate ? ' checked' : '', '/>';
+    switch ($column) {
+      CASE 'activated':
+        echo $activate ? 'Oui' : 'Non';
+        BREAK;
+
+      CASE 'wallet':
+        $wallet = get_field('wallet', $post_id);
+        echo intval($wallet);
+        BREAK;
     }
   }, 10, 2);
 
